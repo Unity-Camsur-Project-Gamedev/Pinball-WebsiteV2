5879dd628cda3bf35e24ed1249512a5d94b0481a
matt lim <matt.lim@breddas.com.ph>
2023-12-01 10:58:36 +0800

    iframe changed

--- src/layout/LiveStreamFrame.js
+++ src/layout/LiveStreamFrame.js
@@ -42,7 +42,6 @@ const LiveStreamFrame = () => {
         // We'll use the padding bottom technique to maintain 16:9 ratio
         className="absolute w-full h-full"
         allow="fullscreen"
-        // src="https://www.youtube.com/embed/dQw4w9WgXcQ"
         width="1280"
         height="720"
         src="https://demo.nanocosmos.de/nanoplayer/embed/1.3.3/nanoplayer.html?group.id=85cafc04-d52d-4bd1-970d-540dfd706832&options.adaption.rule=deviationOfMean2&startIndex=0&playback.latencyControlMode=classic"
