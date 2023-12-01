2541c90b0b5063b28691eb0c49d11f2ba49b334d
matt lim <matt.lim@breddas.com.ph>
2023-12-01 11:02:13 +0800

    iframe updated

--- src/layout/MobileResponsive2.js
+++ src/layout/MobileResponsive2.js
@@ -108,8 +108,6 @@ function MobileResponsive2({ betStatus, empty, setEmpty }) {
           //We'll use the padding bottom technique to maintain 16:9 ratio
           className=" absolute w-full h-full"
           allow="fullscreen"
-          // width="1280"
-          // height="720"
           src="https://demo.nanocosmos.de/nanoplayer/embed/1.3.3/nanoplayer.html?group.id=85cafc04-d52d-4bd1-970d-540dfd706832&options.adaption.rule=deviationOfMean2&startIndex=0&playback.latencyControlMode=classic"
         ></iframe>
       </div>
